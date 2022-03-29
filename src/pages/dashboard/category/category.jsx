aeb76e9616a60003ed1136fdb082803a2768f584
Howard Ding <howardding2000@gmail.com>
2022-03-29 21:55:02 +0000

    code fomat

--- src/pages/dashboard/category/category.jsx
+++ src/pages/dashboard/category/category.jsx
@@ -1,84 +1,82 @@
 import React, { useCallback, useEffect, useState, useRef } from "react";
 import { Card, Button, Table, Space, message, Modal } from "antd";
-import { ArrowRightOutlined } from "@ant-design/icons";
-import { PlusOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined, PlusOutlined } from "@ant-design/icons";
 import LinkButton from "../../../compoments/ui/LinkButton";
 import AddCategoryForm from "../../../compoments/dashboard/content/AddCategoryForm";
 import UpdateCategoryForm from "../../../compoments/dashboard/content/UpdateCategoryForm";
-import "./category.less";
 import {
   reqCategories,
   reqUpdateCategory,
   reqAddCategory,
 } from "../../../api/index";
 
+import "./category.less";
+
 const Category = () => {
   const [categories, setCategories] = useState();
   const [subCategories, setSubCategories] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  const [parent, setParent] = useState({ parentId: "0", parentName: "" });
-  // showStatus: 0 = not show ,1 = show 'Add Category',2 = show 'Update Category'
-  const [showStatus, setShowStatus] = useState(0);
+  const [{ parentId, parentName }, setParent] = useState({
+    parentId: "0",
+    parentName: "",
+  });
+  // showModalStatus: 0 = not show ,1 = show 'Add Category',2 = show 'Update Category'
+  const [showModalStatus, setShowModalStatus] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState();
+  const catRef = useRef();
   const columnsRef = useRef();
-
   const formRef = React.createRef();
 
+  // show first level category list
+  const showCategories = () => {
+    // update state
+    setParent({ parentId: "0", parentName: "" });
+  };
+
+  // show sub category list
   const showSubCategories = (category) => {
     // update state
     setParent({ parentId: category._id, parentName: category.name });
   };
 
-  // get First level category list
-  const getCategories = () => {
-    // update state
-    setParent({ parentId: "0", parentName: "" });
+  // Modal handler
+  const openAddModal = () => setShowModalStatus(1);
+
+  const showUpateModal = useCallback((category) => {
+    // store category id and name for updating Modal
+    setSelectedCategory(category);
+    setShowModalStatus(2);
+  }, []);
+
+
+  const addCategory = () => {
+    setShowModalStatus(0);
   };
 
-  const getCategory = async (parentId) => {
-    setIsLoading(true);
-    const result = await reqCategories(parentId);
-    setIsLoading(false);
+  // update Category's name
+  const updateCategory = async (e) => {
+    setShowModalStatus(0);
+
+    const categoryId = selectedCategory._id;
+    const categoryName = formRef.current.getFieldValue("cat_name");
+
+    const result = await reqUpdateCategory({ categoryId, categoryName });
     if (result.status === 0) {
-      // get category list
-      const categories = result.data;
-      if (parentId === "0") {
-        setCategories(
-          categories.map((item) => {
-            item.key = item._id;
-            return { ...item, key: item._id };
-          })
-        );
-      } else {
-        setSubCategories(
-          categories.map((item) => {
-            item.key = item._id;
-            return { ...item, key: item._id };
-          })
-        );
-      }
-    } else {
-      message.error("Fetch categories error!");
+      message.info("Update successfully!");
+      //reflesh categories
+      showCategories();
     }
   };
 
-  const openAddModel = () => setShowStatus(1);
+  // handle Modal cancel event
+  const handleCancel = () => {
+    setShowModalStatus(0);
+  };
 
-  const showUpateModal = useCallback((category) => {
-    setSelectedCategory(category);
-    // formRef.current.setFildsValue({ cat_name: category.name });
-    // formRef.current.categoryId = category._id;
-    setShowStatus(2);
-  }, []);
-
-  const extra = (
-    <Button onClick={openAddModel}>
-      <PlusOutlined />
-      Add
-    </Button>
-  );
-
-  const initColumns = useCallback(() => {
+  // load Category data and Initialize the Table
+  useEffect(() => {
+    
+  // Initialize columns of <Table>, and stroe it into a Ref. Because it will remain constant throughout the life of the component
     columnsRef.current = [
       {
         title: "Name",
@@ -95,7 +93,8 @@ const Category = () => {
               Update
             </LinkButton>
             {/* how to pass param to event function */}
-            {parent.parentId === "0" && (
+            {/* only show 'Sub category' in First Level */}
+            {parentId === "0" && (
               <LinkButton onClick={() => showSubCategories(category)}>
                 Sub Category
               </LinkButton>
@@ -104,50 +103,70 @@ const Category = () => {
         ),
       },
     ];
-  }, [parent.parentId, showUpateModal]);
 
-  const addCategory = () => {
-    setShowStatus(0);
-  };
+    // fetch category list
+    const getCategory = async (id) => {
+      setIsLoading(true);
 
-  // update Category
-  const updateCategory = async (e) => {
-    console.log(formRef);
-    setShowStatus(0);
-    const categoryId = selectedCategory._id;
-    const categoryName = formRef.current.getFieldValue("cat_name");
-    console.log(categoryId, categoryName);
-    const result = await reqUpdateCategory({ categoryId, categoryName });
-    if (result.status === 0) {
-      console.log("updat successfully!");
-      //reflesh categories
-      getCategories();
-    }
-  };
+      // send request and get result
+      const result = await reqCategories(id);
 
-  const handleCancel = () => {
-    setShowStatus(0);
-  };
+      setIsLoading(false);
 
-  useEffect(() => {
-    initColumns();
-    getCategory(parent.parentId);
-  }, [initColumns, parent]);
+      if (result.status === 0) {
+        const categories = result.data;
 
-  const title = parent.parentName ? (
+        // store first level category list
+        if (parentId === "0") {
+          setCategories(
+            categories.map((item) => {
+              item.key = item._id;
+              return { ...item, key: item._id };
+            })
+          );
+        } else {
+          // store sub category list
+          setSubCategories(
+            categories.map((item) => {
+              item.key = item._id;
+              return { ...item, key: item._id };
+            })
+          );
+        }
+      } else {
+        message.error("Fetch categories error!");
+      }
+    };
+
+    getCategory(parentId);
+    
+  }, [parentId]);
+
+  // Card title and extra setup
+  const title = parentName ? (
     <>
-      <LinkButton onClick={getCategories}>First level category list</LinkButton>{" "}
+      <LinkButton onClick={showCategories}>
+        First level category list
+      </LinkButton>{" "}
       <ArrowRightOutlined style={{ fontSize: "0.8rem", margin: "2px 2px" }} />
-      {parent.parentName}
+      {parentName}
     </>
   ) : (
     "First level category list"
   );
 
+  // set +Add Button
+  const extra = (
+    <Button onClick={openAddModal}>
+      <PlusOutlined />
+      Add
+    </Button>
+  );
+
   return (
     <Card title={title} extra={extra}>
       <Table
-        dataSource={parent.parentId === "0" ? categories : subCategories}
+        dataSource={parentId === "0" ? categories : subCategories}
         columns={columnsRef.current}
         loading={isLoading}
         bordered
@@ -156,17 +175,17 @@ const Category = () => {
 
       <Modal
         title='Add Category'
-        visible={showStatus === 1}
+        visible={showModalStatus === 1}
         onOk={addCategory}
         onCancel={handleCancel}
         destroyOnClose={true}
       >
-        <AddCategoryForm categories={categories} parentId={parent.parentId} />
+        <AddCategoryForm categories={categories} parentId={parentId} />
       </Modal>
 
       <Modal
         title='Update Category'
-        visible={showStatus === 2}
+        visible={showModalStatus === 2}
         onOk={updateCategory}
         onCancel={handleCancel}
         destroyOnClose={true}
